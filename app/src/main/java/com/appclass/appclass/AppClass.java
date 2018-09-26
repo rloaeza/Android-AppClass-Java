ddfb6182e2c7c46e82dc356515a71f8ee5466617
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2018-09-26 11:23:59 -0500

    Initial commit

---
+++ app/src/main/java/com/appclass/appclass/AppClass.java
@@ -0,0 +1,13 @@
+package com.appclass.appclass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_class);
+    }
+}
