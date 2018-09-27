518c1a5becd9b4934edd0feed3d2d5baf32236ce
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2018-09-27 15:10:13 -0500

    Trabajando con la pantalla de inicio de sesion

---
+++ app/src/main/java/com/appclass/appclass/ItemClase.java
@@ -0,0 +1,57 @@
+package com.appclass.appclass;
+//http://www.hermosaprogramacion.com/2014/10/android-listas-adaptadores/
+public class ItemClase {
+    private String nombreClase;
+    private String descripcion;
+    private String codigo;
+    private boolean activa;
+    private String cantidadAlumnos;
+
+    public ItemClase(String nombreClase, String descripcion, String codigo, boolean activa, String cantidadAlumnos) {
+        this.nombreClase = nombreClase;
+        this.descripcion = descripcion;
+        this.codigo = codigo;
+        this.activa = activa;
+        this.cantidadAlumnos = cantidadAlumnos;
+    }
+
+    public String getNombreClase() {
+        return nombreClase;
+    }
+
+    public void setNombreClase(String nombreClase) {
+        this.nombreClase = nombreClase;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public boolean isActiva() {
+        return activa;
+    }
+
+    public void setActiva(boolean activa) {
+        this.activa = activa;
+    }
+
+    public String getCantidadAlumnos() {
+        return cantidadAlumnos;
+    }
+
+    public void setCantidadAlumnos(String cantidadAlumnos) {
+        this.cantidadAlumnos = cantidadAlumnos;
+    }
+}
